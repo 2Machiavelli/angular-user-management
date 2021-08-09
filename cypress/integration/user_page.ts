9156b6085ded808bea9d373e5dd06d311feef2fd
2Machiavelli <stas.chetyrbok@mail.ru>
2021-08-10 01:41:06 +0300

    TESTS: uptate e2e tests

--- cypress/integration/user_page.ts
+++ cypress/integration/user_page.ts
@@ -1,6 +1,20 @@
-describe("Rating", () => {
+describe("User Page", () => {
   beforeEach(() => {
     cy.visit('/users/9f07341f-c7e6-45b7-bab0-af6de5a4582d')
+  })
+
+  it("has the correct title", () => {
+    cy.title().should("equal", "AUM | Louane Vidal")
+  })
+
+  it("check if the side menu works correctly", () => {
+    // Open
+    cy.get(".app__menu-btn").click()
+    cy.get(".drawer").should("be.visible")
+  
+    // Close
+    cy.get(".app__menu-btn").click()
+    cy.get(".drawer").should("be.hidden")
   })
 
   it("Increase the rating", () => {
